eb12ae04814b464857ce5450d40a1370965fc7ee
masafumikazama <masa@codecabin.com.au>
2021-04-29 17:59:39 +1000

    second commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn'
+  }
+}
